7271eda5bda9e0755134b8d3f25ad397e846af3c
T-107 <T-107@PC245>
2015-07-13 19:42:54 -0500

    clase13715

--- src/collections/GeneradorDeusuarios.java
+++ src/collections/GeneradorDeusuarios.java
@@ -13,13 +13,19 @@ import java.util.List;
  * @author T-107
  */
 public class GeneradorDeusuarios {
+    /*Los collection tienen el potencial de usarse para construir prototipos de base de datos NoSQL
+    otra caracteristica de los collections es que tienen clases y metodos de ordenamiento*/
     private List<Usuario> usuarios;
     public GeneradorDeusuarios(){
         Usuario u1=new Usuario("Cris", 20, "cris_ctjmvr@hotmail.com");
         Usuario u2=new Usuario("jess", 19, "axojes@hotmail.com");
         Usuario u3=new Usuario("Pepe", 22, "pepe@gmail.com");
-        
+        Usuario u4=new Usuario("Pamcha",60,"pancha@hotmail.com");
         usuarios=new ArrayList<Usuario>();
+        usuarios.add(u1);
+        usuarios.add(u2);
+        usuarios.add(u3);
+        usuarios.add(u4);
     }
 
     public List<Usuario> getUsuarios() {
@@ -28,5 +34,10 @@ public class GeneradorDeusuarios {
 
     public void setUsuarios(List<Usuario> usuarios) {
         this.usuarios = usuarios;
+    }
+    public String agregarUsuario(String nombre, int edad, String email){
+        Usuario u=new Usuario(nombre, edad, email);
+        usuarios.add(u);
+        return "Usuario agregado con exito";
     }
 }
