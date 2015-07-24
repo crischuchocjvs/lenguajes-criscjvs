c9ef023b58b464cfc35fe56ba54e22ca4e57a0d2
T-107 <T-107@PC245>
2015-07-23 19:43:15 -0500

    23/07/15

--- src/serializacion/ModeloPersistenciaAnimal.java
+++ src/serializacion/ModeloPersistenciaAnimal.java
@@ -6,13 +6,31 @@
 package serializacion;
 
 import java.io.*;
+import java.util.ArrayList;
 public class ModeloPersistenciaAnimal {
+   String ruta="D:\\animalitos.cris"; 
+   ArrayList<Animal> animales;
+   public ModeloPersistenciaAnimal(){
+       animales=new ArrayList<Animal>();
+   }
     public void guardar (Animal animal)throws Exception{
         //paso1 crear el archivo donde se guaradara el animal
-        File file=new  File("D:\\animalitos.cris");
+        File file=new  File(ruta);
+        if(file.exists()){
+            animales=buscarTodos();
+        }
         FileOutputStream fos=new FileOutputStream(file);
         ObjectOutputStream oos=new ObjectOutputStream(fos);
-        oos.writeObject(animal);
+        animales.add(animal);
+        oos.writeObject(animales);
         System.out.println("Animalito enlatado listo para llevar");
+    }
+    public ArrayList<Animal> buscarTodos()throws Exception{
+        Animal a=new Animal();
+        File f =new File(ruta);
+        FileInputStream fis=new FileInputStream(f);
+        ObjectInputStream ois=new ObjectInputStream(fis);
+                animales=(ArrayList<Animal>) ois.readObject();
+                return animales;
     }
 }
